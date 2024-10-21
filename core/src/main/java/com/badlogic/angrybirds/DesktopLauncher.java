0333d3cb2ad4e3ec8e25e1697a618fdf1927ff8d
Riya Jain <riya23441@iiitd.ac.in>
2024-10-21 17:29:28 +0530

    Did more changes

--- core/src/main/java/com/badlogic/angrybirds/DesktopLauncher.java
+++ core/src/main/java/com/badlogic/angrybirds/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
     public static void main (String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setForegroundFPS(60);
-        config.setTitle("Tank Stars");
+        config.setTitle("Angry Birds");
         new Lwjgl3Application(new AngryBirds(), config);
     }
 }
